a5d72e1a8fa9065c09a21451512b6f80d32c80d9
Simbiat <ganiyusimbiat@gmail.com>
2019-01-20 10:47:35 +0100

    Initial Commit

---
+++ challenge.js
@@ -0,0 +1,27 @@
+/* area of a parallelogram */
+
+function areaOfParallelogram(breadth, height) {
+	return breadth * height;
+
+}
+console.log(areaOfParallelogram(6, 3))
+
+
+/* the volume of a cylinder */
+
+function volumeOfCylinder(radius, height) {
+	const pi = 3.142;
+	return pi * (radius * radius) * height;
+}
+
+console.log(volumeOfCylinder(5, 9))
+
+
+
+/* area of a rectangle */
+
+function areaOfRectangle(width, height) {
+	return width * height;
+}
+
+console.log(areaOfRectangle(10, 8))
